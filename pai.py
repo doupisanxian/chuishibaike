3039baa4d52fbccdc5ff2017b6af989bdfd2496d
doupisanxian <shijieend@hotmail.com>
2018-04-20 16:34:43 +0800

    ceshi1

---
+++ pai.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+content = requests.get('https://www.qiushibaike.com/').content
+soup = BeautifulSoup(content,'html.parser')
+
+for div in soup.find_all('div',{'class':'content'}):
+    print div.text.strip()
+
